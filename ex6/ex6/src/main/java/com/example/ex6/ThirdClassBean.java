3d3efcafa3085796501fe65fd6c3a3a924f524e0
Paloma Rebolleda <paloma.rebolleda@gmail.com>
2021-12-20 18:54:32 +0100

    trying to fix override....

--- ex6/ex6/src/main/java/com/example/ex6/ThirdClassBean.java
+++ ex6/ex6/src/main/java/com/example/ex6/ThirdClassBean.java
@@ -9,13 +9,13 @@ import org.springframework.stereotype.Component;
 public class ThirdClassBean {
     @Bean
     CommandLineRunner thirdBeanExcuted(){
-        return thirdBean -> {
+        return command -> {
             System.out.println("Hola soy el tercer Bean");
         };
 
         }
 
-    //@Override
+       // @Override
     public void run (String...args) throws Exception {
         System.out.println("Bean argument" + args);
     }
